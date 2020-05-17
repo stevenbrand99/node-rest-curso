e82269f445b6c85896a3f4961cb1448311c74834
Steven <steven.brand1999@gmail.com>
2020-05-17 17:59:36 -0500

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,41 @@
+
+require('./config/config')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.post("/usuario", (req, res) => {
+	let body = req.body;
+
+	if (body.nombre === undefined)
+	{
+		res.status(400).json({
+			ok: false,
+			mensaje : "El nombre es necesario"
+		})
+	}
+	else
+	{
+		res.json({
+			persona: body
+		});
+	}
+
+})
+
+app.put("/usuario/:id", function (req, res) {
+  let id = req.params.id;
+  res.json({
+    id,
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Escuchando en puerto: ", process.env.PORT);
+});
